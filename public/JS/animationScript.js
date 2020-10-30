ce78ca8231dbcd32205dc60afc77579df248ad36
parth-jaiswal <55652727+parth-jaiswal@users.noreply.github.com>
2020-10-30 15:13:14 +0530

    Minor Changes

---
+++ public/JS/animationScript.js
@@ -0,0 +1,27 @@
+document.getElementsByName('who')[0].addEventListener('onmouseenter', function(){
+	this.style.color = blue;
+})
+
+
+// $(document).ready(function() {
+	
+// 	var duration = 300;
+// 	$(".buttons").mouseover(function () {
+// 			$(this).stop(true).animate(
+// 				{
+// 					backgroundColor: "#1a00e0",
+// 					color: "#fff",
+// 				},
+// 				duration
+// 			);
+// 		})
+// 		.on("mouseout", function () {
+// 			$(this).stop(true).animate(
+// 				{
+// 					backgroundColor: "#fff",
+// 					color: "#ebc000",
+// 				},
+// 				duration
+// 			);
+// 		});
+// 	})
